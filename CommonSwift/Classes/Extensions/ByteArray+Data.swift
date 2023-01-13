45608ac881532a788f1a6db9af510906a9960be4
TigranIskandaryan <tiger@sublab.dev>
2023-01-14 02:23:06 +0300

    Added the missing comments

--- CommonSwift/Classes/Extensions/ByteArray+Data.swift
+++ CommonSwift/Classes/Extensions/ByteArray+Data.swift
@@ -1,6 +1,8 @@
 import Foundation
 
 extension Array where Element == UInt8 {
+    /// `Data` from the array
+    /// - Returns: `Data` from the array
     public func toData() -> Data {
         Data(self)
     }
