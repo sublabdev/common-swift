07256a33df242d977c743990f01459ed85fa5e72
Alex Oakley <alex@alexoakley.com>
2023-01-14 02:09:20 -0800

    Added runBlocking as in kotlin

---
+++ CommonSwift/Classes/Numerics/Helpers/RunBlocking.swift
@@ -0,0 +1,56 @@
+// from https://gist.github.com/sainecy/4366a1b99c7317fac63bfeb19d1cfab2
+import Foundation
+
+private final class RunBlocking<T, Failure: Error> {
+    fileprivate var value: Result<T, Failure>? = nil
+}
+
+extension RunBlocking where Failure == Never {
+    func runBlocking(_ operation: @Sendable @escaping () async -> T) -> T {
+        Task {
+            let task = Task(operation: operation)
+            self.value = await task.result
+        }
+        DispatchQueue.global().sync {
+            while value == nil {
+                RunLoop.current.run(mode: .defaultRunLoopMode, before: .distantFuture)
+            }
+        }
+        switch value {
+        case let .success(value):
+            return value
+        case .none:
+            fatalError("Run blocking not received value")
+        }
+    }
+}
+
+extension RunBlocking where Failure == Error {
+    func runBlocking(_ operation: @Sendable @escaping () async throws -> T) throws -> T {
+        Task {
+            let task = Task(operation: operation)
+            value = await task.result
+        }
+        DispatchQueue.global().sync {
+            while value == nil {
+                RunLoop.current.run(mode: .defaultRunLoopMode, before: .distantFuture)
+            }
+        }
+        switch value {
+        case let .success(value):
+            return value
+        case let .failure(error):
+            throw error
+        case .none:
+            fatalError("Run blocking not received value")
+        }
+    }
+}
+
+func runBlocking<T>(@_implicitSelfCapture _ operation: @Sendable @escaping () async -> T) -> T {
+    RunBlocking().runBlocking(operation)
+}
+
+func runBlocking<T>(@_implicitSelfCapture _ operation: @Sendable @escaping () async throws -> T) throws -> T {
+    try RunBlocking().runBlocking(operation)
+}
